ae8dcd46ed84d06aa4d98fc1e2f43a840ec69364
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-12-21 11:57:57 -0300

    feat: delete url by id

--- src/controllers/urls.Controller.js
+++ src/controllers/urls.Controller.js
@@ -35,11 +35,6 @@ export async function shortener(req, res){
 export async function searchUrl(req, res){
     const {id} = req.params;
 
-    if(!id){
-        res.sendStatus(404)
-        return
-    }
-
     try{
         const url = await connection.query(`SELECT id, "shortUrl", url FROM urls WHERE id = $1`, [id])
 
@@ -57,9 +52,19 @@ export async function searchUrl(req, res){
 }
 
 export async function redirect(req, res){
+    const {shortUrl} = req.params;
 
     try{
+        const url = await connection.query(`SELECT url, "visitCount" FROM urls WHERE "shortUrl" = $1`, [shortUrl])
 
+        if(url.rows.length === 0){
+            res.sendStatus(404)
+            return
+        }
+
+        await connection.query(`UPDATE urls SET "visitCount" = $1 WHERE "shortUrl" = $2`, [url.rows[0].visitCount+1, shortUrl])
+
+        res.redirect(url.rows[0].url)
 
     }catch(err){
         console.log(err);
@@ -68,9 +73,38 @@ export async function redirect(req, res){
 }
 
 export async function deleteUrl(req, res){
+    const {id} = req.params;
+    const {authorization} = req.headers;
+    const token = authorization.replace("Bearer ", "");
+
+    if(!token){
+        res.sendStatus(401);
+        return;
+    }
 
     try{
+        const section = await connection.query("SELECT * FROM sections WHERE token = $1", [token])
 
+        if(section.rows.length === 0){
+            res.sendStatus(401)
+            return
+        }
+
+        const url = await connection.query(`SELECT "userId" FROM urls WHERE id = $1`, [id])
+
+        if(url.rows.length === 0){
+            res.sendStatus(404)
+            return
+        }
+
+        if(section.rows[0].userId !== url.rows[0].userId){
+            res.sendStatus(401)
+            return
+        }
+
+        await connection.query("DELETE FROM urls WHERE id = $1", [id])
+
+        res.sendStatus(204)
         
     }catch(err){
         console.log(err);
