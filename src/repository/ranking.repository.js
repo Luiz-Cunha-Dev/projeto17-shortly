e5a497a3695ff940002bc75ca6ca1023039864a9
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-12-23 17:50:53 -0300

    fix: ranking order

--- src/repository/ranking.repository.js
+++ src/repository/ranking.repository.js
@@ -1,5 +1,5 @@
 import { connection } from "../database/db.js";
 
 export function getRankingList(){
-    return connection.query(`SELECT users.id, users.name, COUNT(urls."userId") AS "linksCount", SUM(urls."visitCount") AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10`)
+    return connection.query(`SELECT users.id, users.name, COUNT(urls."userId") AS "linksCount", SUM(COALESCE(urls."visitCount", 0)) AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10`)
 }
